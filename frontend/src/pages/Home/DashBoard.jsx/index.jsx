62c20d1190f60c1ece81f9f7a369bfd44a7e9fb7
AkhilendreSinghRawat <akhilendre.rawat@gamil.com>
2022-11-28 17:50:02 +0530

    dashboard initialized

--- frontend/src/pages/Home/DashBoard.jsx/index.jsx
+++ frontend/src/pages/Home/DashBoard.jsx/index.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+      <div
+        style={{
+          fontSize: 'xx-large',
+          color: 'darkcyan',
+          margin: '2vh 2vw',
+          fontWeight: 'bold',
+        }}
+      >
+        Dashboard
+      </div>
+      <div
+        style={{
+          borderBottom: 'solid 1px lightgray',
+          margin: '1vh 1vw',
+        }}
+      />
+    </div>
   )
 }
 
